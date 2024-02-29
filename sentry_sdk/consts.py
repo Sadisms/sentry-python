a34ffb41d610fb27d3af6a135e281740ccc1d277
Дмитрий <madara@deeplay.io>
2024-02-29 16:04:04 +0600

    Add default http transport disable ssl

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -1,3 +1,4 @@
+import sentry_sdk
 from sentry_sdk._types import TYPE_CHECKING
 
 # up top to prevent circular import due to integration import
@@ -253,7 +254,7 @@ class ClientConstructor(object):
         in_app_exclude=[],  # type: List[str]  # noqa: B006
         default_integrations=True,  # type: bool
         dist=None,  # type: Optional[str]
-        transport=sentry_sdk.transport.DEFAULT_HTTP_TRANSPORT,  # type: Optional[Union[sentry_sdk.transport.Transport, Type[sentry_sdk.transport.Transport], Callable[[Event], None]]]
+        transport=DEFAULT_HTTP_TRANSPORT,  # type: Optional[Union[sentry_sdk.transport.Transport, Type[sentry_sdk.transport.Transport], Callable[[Event], None]]]
         transport_queue_size=DEFAULT_QUEUE_SIZE,  # type: int
         sample_rate=1.0,  # type: float
         send_default_pii=False,  # type: bool
